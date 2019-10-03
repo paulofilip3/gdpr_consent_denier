8033cbae8289caf63d909f93d513ca377a3c884e
Paulo Rosário <paulo.filip3@gmail.com>
2019-10-03 14:08:20 +0200

    Remove leading slashes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-![logo](/src/img/gdpr_lock_128.png "by Mohamed Hassan (https://pixabay.com/users/mohamed_hassan-5229782)")
+![logo](src/img/gdpr_lock_128.png "by Mohamed Hassan (https://pixabay.com/users/mohamed_hassan-5229782)")
 # GDPR Consent Denier
 
 A chrome extension to automatically deny consent for common GDPR cookie banners.
--- src/js/inject.js
+++ src/js/inject.js
@@ -2,6 +2,6 @@
     var body = document.getElementsByTagName('body')[0];
     var script = document.createElement('script');
     script.setAttribute('type', 'text/javascript');
-    script.setAttribute('src', chrome.extension.getURL('/src/js/denier.js'));
+    script.setAttribute('src', chrome.extension.getURL('src/js/denier.js'));
     body.appendChild(script);
 })();
